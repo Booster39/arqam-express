00eff81a45a344308dd6840371ece363c6ebbb23
Doucoure <abdoulayediame.doucoure@gmail.com>
2024-06-05 19:14:51 +0200

    feat: associate one group to its hours

--- myapp/lastHourOfGroup.ts
+++ myapp/lastHourOfGroup.ts
@@ -1,3 +1,5 @@
+import { group } from "console";
+
 const fs = require("fs");
 const papa = require("papaparse");
 
@@ -41,10 +43,48 @@ const getGroups = (dayAbrev: string) => {
     }
   }
   groupsOfDay = [...new Set(groupsOfDay)];
+
   return groupsOfDay;
 };
 
+const getHours = (dayAbrev: string) => {
+  let groupsOfDay: string[] = [];
+  let hours: Object[] = [];
+  const fields = Object.keys(parsedData.data[0]).filter((name) =>
+    name.startsWith(dayAbrev)
+  );
+
+  for (const row of parsedData.data) {
+    for (const f of fields) {
+      const value = row[f];
+
+      if (value) {
+        const groups: string[] = value.split(", ");
+
+        groupsOfDay.push(...groups);
+      }
+    }
+  }
+  groupsOfDay = [...new Set(groupsOfDay)];
+  for (const row of parsedData.data) {
+    for (const f of fields) {
+      const value = row[f];
+      if (value) {
+        groupsOfDay.forEach((group) => {
+          if (value.includes(group)) {
+            console.log(`${group}: ${f}`);
+            hours.push({ group, f });
+          }
+        });
+      }
+    }
+  }
+  return hours;
+};
+
 let groupsLundi: string[] = getGroups("Lun");
+let hoursLundi: Object[] = getHours("Lun");
+
 let groupsMardi: string[] = getGroups("Mar");
 let groupsMecredi: string[] = getGroups("Mer");
 let groupsJeudi: string[] = getGroups("Jeu");
